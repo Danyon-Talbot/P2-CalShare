d9d50852bbbe168e3145042ccf16d557b04a0510
Emma Denton <emmad@192-168-1-200.tpgi.com.au>
2023-11-23 18:53:28 +1100

    inital code

---
+++ seeds/index.js
@@ -0,0 +1,11 @@
+;
+
+const sequelize = require('../config/connection');
+
+
+const seedAll = async () => {
+
+  process.exit(0);
+};
+
+seedAll();
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const routes = require('./routes');
+const { Sequelize } = require('sequelize');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_DATABASE;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
+  dialect: 'mysql',
+});
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+sequelize.sync()
+  .then(() => {
+    console.log('Database synchronized successfully');
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error syncing the database:', err);
+  });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
