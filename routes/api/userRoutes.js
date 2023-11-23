c21da9a9ef9e9af936b45ef1b0cb712072191854
Van Gogh <139753682+CircuitSorcerer@users.noreply.github.com>
2023-11-23 23:38:40 +1100

    Added test seeds, Created user API route files

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const User = require('../../models/User');
+
+// GET all users
+router.get('/', async (req, res) => {
+    try {
+      const userData = await User.findAll();
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  // CREATE a new user
+router.post('/', async (req, res) => {
+    try {
+      const userData = await User.create(req.body);
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  
