a93170677265c8bc11ede3dc7be194818beb64a2
danyon talbot <danyon.talbot2@gmail.com>
2023-11-24 19:15:52 +1100

    Added Handlebars Login Page Code

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const routes = require('./routes');
 const { Sequelize } = require('sequelize');
 const mysql = require('mysql2');
 require('dotenv').config();
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
@@ -14,9 +16,28 @@ const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   dialect: 'mysql',
 });
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.set('views', path.join(__dirname, 'views'));
+
+// renders the login page
+app.get('/login', function (req, res) {
+  res.render('login', { title: 'CalShare'});
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+
+
 
 sequelize.sync()
   .then(() => {
@@ -31,10 +52,6 @@ sequelize.sync()
 
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(routes);
 
 // need to chat with whoever has been working on server.js
 // this is meant to Synchronize the Database and start the application
