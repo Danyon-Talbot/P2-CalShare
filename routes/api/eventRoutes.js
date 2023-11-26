343174f0149814f157b9f67884dd88ce89af5f5b
Van Gogh <139753682+CircuitSorcerer@users.noreply.github.com>
2023-11-27 02:47:36 +1100

    Filled out event and user routes

---
+++ routes/api/eventRoutes.js
@@ -0,0 +1,68 @@
+const router = require('express').Router();
+const Event = require('../../models/Event');
+
+
+//POST
+router.post('/event', async (req, res) => {
+    try {
+      const eventData = await Event.create(req.body);
+      res.status(200).json(eventData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+//GET
+router.get('/event', async (req, res) => {
+    try {
+      const eventData = await Event.findAll();
+      res.status(200).json(eventData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+//PUT
+router.put('/event/:EventID', async (req, res) => {
+    try {
+        const EventID = parseInt(req.params.id);
+        const updatedData = req.body;
+
+        const eventIndex = Event.findIndex(event => event.id === EventID);
+
+        if (eventIndex === -1) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        events[eventIndex] = { ...events[eventIndex], updatedData };
+
+        res.json({ message: 'Event updated successfully', event: events[eventIndex] });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+})
+
+//DELETE
+router.delete('/event/:EventID', async (req, res) => {
+    try {
+        const EventID = parseInt(req.params.id);
+
+        const eventIndex = Event.findIndex(event => event.id === EventID);
+
+        if (eventIndex === -1) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const deletedEvent = events.splice(eventIndex, 1)[0];
+
+        res.json({ message: 'Event deleted successfully', event: deletedEvent });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+
+    }
+});
+
+
+module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,23 +2,45 @@ const router = require('express').Router();
 const User = require('../../models/User');
 
 // GET all users
-router.get('/', async (req, res) => {
-    try {
-      const userData = await User.findAll();
-      res.status(200).json(userData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+router.get('/users', async (req, res) => {
+  try {
+    const userData = await User.findAll();
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-  // CREATE a new user
-router.post('/', async (req, res) => {
-    try {
-      const userData = await User.create(req.body);
-      res.status(200).json(userData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+// CREATE a new user
+router.post('/users', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-  
+// DELETE
+router.delete('/users/:UserID', async (req, res) => {
+  try {
+    const userId = req.params.UserID;
+    await User.destroy({ where: { id: userId } });
+    res.status(200).json({ message: 'User deleted successfully' })
+  } catch {
+    res.status(400).json(err);
+  }
+});
+
+// PUT
+router.put('/users/:userID', async (req, res) => {
+  try {
+    const userId = req.params.userID;
+    const updatedUserData = await User.update(req.body, { where: { id: userId } });
+    res.status(200).json(updatedUserData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router
