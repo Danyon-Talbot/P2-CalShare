3d80afaaf07d7badf0136687197b789422d7148f
danyon talbot <danyon.talbot2@gmail.com>
2023-11-28 19:14:03 +1100

    Various Bug Fixes Throughout the Code

--- db/schema.sql
+++ db/schema.sql
@@ -2,11 +2,16 @@ DROP DATABASE IF EXISTS calendar_db;
 
 CREATE DATABASE calendar_db;
 
+USE calendar_db;
+
 CREATE TABLE User (
     id INT AUTO_INCREMENT PRIMARY KEY,
     firstname VARCHAR(30) NOT NULL,
     lastname VARCHAR(30) NOT NULL,
-    password VARCHAR(30) NOT NULL
+    email VARCHAR(30) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Event (
@@ -21,6 +26,7 @@ CREATE TABLE UserEvent (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     event_id INT NOT NULL,
+    user_role VARCHAR(30) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES User(id),
     FOREIGN KEY (event_id) REFERENCES Event(id)
 );
---
+++ routes/api/eventRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+// const bcrypt = require('bcrypt');
+const { Event } = require('../../models');
+
+
+// // GET all users
+// router.get('/', async (req, res) => {
+//     try {
+//         const events = await Event.findAll();
+//         res.json(events);
+//     } catch (error) {
+//         console.error('Error fetching events:', error);
+//         res.status(500).json({ message: 'Error fetching events' });
+//     }
+// });
+
+// // GET user by ID
+// router.get('/:id', async (req, res) => {
+//     const eventId = req.params.id;
+
+//     try {
+//         const event = await Event.findByPk(eventId);
+
+//         if (event) {
+//             res.json(event);
+//         } else {
+//             res.status(404).json({ message: 'Event not found' });
+//         }
+//     } catch (error) {
+//         console.error('Error fetching event:', error);
+//         res.status(500).json({ message: 'Error fetching event' });
+//     }
+// });
