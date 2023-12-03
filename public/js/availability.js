0613e2388cc76a691fd3fbcbc7d47861a3358786
isaacmasterman <142137796+isaacmasterman@users.noreply.github.com>
2023-12-03 22:23:39 +1100

    debugged the system so availability is displayed on the calendar and on the availability modal

--- public/js/availability.js
+++ public/js/availability.js
@@ -1,22 +1,37 @@
+async function updateCalendarWithAvailability(userId, calendar) {
+    try {
+        const response = await fetch(`/api/availability/${userId}`);
+        if (response.ok) {
+            const availabilityData = await response.json();
+            // Clear existing events and add new ones
+            calendar.getEvents().forEach(event => event.remove());
+            availabilityData.forEach(event => calendar.addEvent(event));
+        } else {
+            console.error('Failed to fetch availability');
+        }
+    } catch (error) {
+        console.error('Error fetching availability:', error);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth', // Default view
+        initialView: 'timeGridWeek', // Default view
+        allDaySlot: false,
         headerToolbar: {
-            left: 'prev,next today',
+            left: 'prev,next',
             center: 'title',
-            right: 'dayGridMonth,timeGridWeek'
+            right: 'today'
         }
     });
     calendar.render();
 
     // Fetch and display events data
-    // API/Availability end point needs to be made
-    fetch('/api/availability')
-        .then(response => response.json())
-        .then(eventsData => {
-            eventsData.forEach(event => {
-                calendar.addEvent(event);
-            });
-        });
+    const userId = sessionStorage.getItem('user_id');
+    if (userId) {
+        updateCalendarWithAvailability(userId, calendar);
+    } else {
+        console.error('User ID not found in session storage');
+    }
 });
